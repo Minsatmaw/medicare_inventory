842738220e41dcb42b428769241106d071975da8
ZarniPhyoe <61306504+ZarniPhyoe@users.noreply.github.com>
2023-07-11 23:52:21 +0630

    Person Edit problem solved

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -8,73 +8,73 @@ use App\Http\Requests\UpdatePersonRequest;
 
 class PersonController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $people = Person::paginate(10);
-        return view('people.index', compact('people'));
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $people = Person::paginate(10);
+    return view('people.index', compact('people'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('people.create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    return view('people.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StorePersonRequest $request)
-    {
-        $request->validate([
-            'name' => 'required|unique:people,name',
-            'slug' => 'required|unique:people,slug',
-        ]);
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StorePersonRequest $request)
+  {
+    $request->validate([
+      'name' => 'required|unique:people,name',
+      'slug' => 'required|unique:people,slug',
+    ]);
 
-        Person::create($request->all());
-        return redirect()->route('people.index')->with('success','Operator Create Success');
-    }
+    Person::create($request->all());
+    return redirect()->route('people.index')->with('success', 'Operator Create Success');
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Person $person)
-    {
-        return view('people.show', compact('person'));
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Person $person)
+  {
+    return view('people.show', compact('person'));
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Person $person)
-    {
-        return view('people.edit', compact('person'));
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Person $person)
+  {
+    return view('people.edit', compact('person'));
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdatePersonRequest $request, Person $person)
-    {
-        $request->validate([
-            'name' => 'required|unique:people,name,' . $person->id,
-            'slug' => 'required|unique:people,slug'. $person->id,
-        ]);
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdatePersonRequest $request, Person $person)
+  {
+    $request->validate([
+      'name' => 'required|unique:people,name',
+      'slug' => 'required|unique:people,slug',
+    ]);
 
-        $person->update($request->all());
-        return redirect()->route('people.index')->with('success','Operator Update Success');
-    }
+    $person->update($request->all());
+    return redirect()->route('people.index')->with('success', 'Operator Update Success');
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Person $person)
-    {
-        $person->delete();
-        return redirect()->route('people.index')->with('success','Operator Delete Success');
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Person $person)
+  {
+    $person->delete();
+    return redirect()->route('people.index')->with('success', 'Operator Delete Success');
+  }
 }
--- resources/views/people/edit.blade.php
+++ resources/views/people/edit.blade.php
@@ -19,8 +19,8 @@
                 
                 <div class="mb-4">
                     <label for="slug" class="block mb-1 font-semibold text-gray-700">Slug</label>
-                    <input type="text" name="slug" id="slug" class="w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('name') border-red-700 is-invalid  @enderror" value="{{ $person->slug }}" required>
-                    @error('name')
+                    <input type="text" name="slug" id="slug" class="w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('slug') border-red-700 is-invalid  @enderror" value="{{ $person->slug }}" required>
+                    @error('slug')
                         <div class="text-red-600 invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
